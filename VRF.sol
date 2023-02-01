1dd61d7c630ecf8ccd2ddc5bdcea5f911b9b147d
Chris <hereokay88@gmail.com>
2023-02-01 18:02:52 +0900

    owner 제거

--- VRF.sol
+++ VRF.sol
@@ -4,22 +4,6 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-/**
- * @notice A Chainlink VRF consumer which uses randomness to mimic the rolling
- * of a 20 sided dice
- */
-
-/**
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
- */
-
-/**
- * THIS IS AN EXAMPLE CONTRACT THAT USES HARDCODED VALUES FOR CLARITY.
- * THIS IS AN EXAMPLE CONTRACT THAT USES UN-AUDITED CODE.
- * DO NOT USE THIS CODE IN PRODUCTION.
- */
-
 contract VRFD20 is VRFConsumerBaseV2 {
     uint256 private constant ROLL_IN_PROGRESS = 42;
 
@@ -31,19 +15,10 @@ contract VRFD20 is VRFConsumerBaseV2 {
     // Goerli coordinator. For other networks,
     // see https://docs.chain.link/docs/vrf-contracts/#configurations
     address vrfCoordinator = 0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
-
-    // The gas lane to use, which specifies the maximum gas price to bump to.
-    // For a list of available gas lanes on each network,
-    // see https://docs.chain.link/docs/vrf-contracts/#configurations
     bytes32 s_keyHash =
         0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
 
-    // Depends on the number of requested values that you want sent to the
-    // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
-    // so 40,000 is a safe default for this example contract. Test and adjust
-    // this limit based on the network that you select, the size of the request,
-    // and the processing of the callback request in the fulfillRandomWords()
-    // function.
+    
     uint32 callbackGasLimit = 40000;
 
     // The default is 3, but you can set this higher.
@@ -62,27 +37,13 @@ contract VRFD20 is VRFConsumerBaseV2 {
     event DiceRolled(uint256 indexed requestId, address indexed roller);
     event DiceLanded(uint256 indexed requestId, uint256 indexed result);
 
-    /**
-     * @notice Constructor inherits VRFConsumerBaseV2
-     *
-     * @dev NETWORK: Goerli
-     *
-     * @param subscriptionId subscription id that this consumer contract can use
-     */
     constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
         COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
         s_owner = msg.sender;
         s_subscriptionId = subscriptionId;
     }
 
-    /**
-     * @notice Requests randomness
-     * @dev Warning: if the VRF response is delayed, avoid calling requestRandomness repeatedly
-     * as that would give miners/VRF operators latitude about which VRF response arrives first.
-     * @dev You must review your implementation details with extreme care.
-     *
-     */
-    function rollDice() public onlyOwner returns (uint256 requestId) {
+    function rollDice() public returns (uint256 requestId) {
         // Will revert if subscription is not set and funded.
         requestId = COORDINATOR.requestRandomWords(
             s_keyHash,
@@ -97,19 +58,7 @@ contract VRFD20 is VRFConsumerBaseV2 {
         emit DiceRolled(requestId, s_owner);
     }
 
-    /**
-     * @notice Callback function used by VRF Coordinator to return the random number to this contract.
-     *
-     * @dev Some action on the contract state should be taken here, like storing the result.
-     * @dev WARNING: take care to avoid having multiple VRF requests in flight if their order of arrival would result
-     * in contract states with different outcomes. Otherwise miners or the VRF operator would could take advantage
-     * by controlling the order.
-     * @dev The VRF Coordinator will only send this function verified responses, and the parent VRFConsumerBaseV2
-     * contract ensures that this method only receives randomness from the designated VRFCoordinator.
-     *
-     * @param requestId uint256
-     * @param randomWords  uint256[] The random result returned by the oracle.
-     */
+    
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] memory randomWords
@@ -142,6 +91,7 @@ contract VRFD20 is VRFConsumerBaseV2 {
         require(users[msg.sender] == 0);
 
         users[msg.sender]= number;
+
         count[number] = count[number] + 1;
     }
 
